aaa014c2b4ec456e7fea256b9cf1aa0ed440b22a
Dino Roca <rocalauented@gmaiil.com>
2022-05-23 22:14:30 -0500

    Header corregido

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  headerMenu: MenuItem[] = [
+    {
+      texto: 'Home',
+      ruta: '/home'
+    },
+    {
+      texto: 'Inferencia estadística',
+      ruta: '/inferencia'
+    },
+    {
+      texto: 'Formulario',
+      ruta: '/formulario'
+    },
+    {
+      texto: 'Calculadora',
+      ruta: '/calculadora'
+    }
+  ];
 
   constructor() { }
 
