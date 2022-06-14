dd6c11aa1cd387ef2fb5d4380ef7288991941952
Mirko <giancarlo124522@gmail.com>
2022-06-14 00:04:16 -0500

    bernoulli

--- src/app/pages/inferencia/dist-importantes/vad-bernoulli/vad-bernoulli.component.ts
+++ src/app/pages/inferencia/dist-importantes/vad-bernoulli/vad-bernoulli.component.ts
@@ -8,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class VadBernoulliComponent implements OnInit {
 
   constructor() { }
+  // ecuaciones
+  Ohmega = '\\Omega';
+  OhmegaF = '\\Omega = \\{E,F\\}';
+  Chi =  '\\Chi';
+  Epsilon ='\\Epsilon';
+  rho ='\\rho';
+  rhoF ='\\rho = \\Rho[ \\Chi = 1]';
+  x = 'x';
+  formula ='0<\\rho<1';
+  q = 'q';
+  qF = 'q = [';
+  qF2 = '= 0 ] = 1 -\\rho';
+  formula2 = 'f(x) = \\Rho[';
+  formula21 = '=x]= \\rho^{x}q^{1-x}';
+  formula3 = 'x = 0,1';
+  formula4 = '\\left. \\begin{array}{rcl}2x+y & = & 1\\ x+y & = & 4 \\ x+y+z & = & 6\\end{array}\\right\\}';
 
   ngOnInit(): void {
   }
