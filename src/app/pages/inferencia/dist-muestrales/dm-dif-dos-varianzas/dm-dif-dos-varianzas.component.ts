a0c0c00e2a84213dbe662e0aebe7504b5ba2cc71
dino-linux <rocalaurented@gmail.com>
2022-06-19 00:30:16 -0500

    Agregado dif. de 2 varianzas

--- src/app/pages/inferencia/dist-muestrales/dm-dif-dos-varianzas/dm-dif-dos-varianzas.component.ts
+++ src/app/pages/inferencia/dist-muestrales/dm-dif-dos-varianzas/dm-dif-dos-varianzas.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DmDifDosVarianzasComponent implements OnInit {
 
+  //Ecuaciones
+  zmay = 'Z';
+  mayig = '\\geq';
+  x1bar = '\\bar{X_1}';
+  x2bar = '\\bar{X_2}';
+  n1 = 'n_1';
+  n2 = 'n_2';
+  n01 = 'N(0, 1)';
+  mu1 = '\\mu_1';
+  mu2 = '\\mu_2';
+  sigma1 = '\\sigma_1';
+  sigma2 = '\\sigma_2';
+  mux1bar = '\\mu_{\\bar{X_1}-\\bar{X_2}} = E(\\bar{X_1}-\\bar{X_2}) =E(\\bar{X_1})-E(\\bar{X_2}) = \\mu_{1}-\\mu_{2}.';
+  sigmax1bar = '\\sigma_{\\bar{X_1}-\\bar{X_2}}^2 = V(\\bar{X_1}-\\bar{X_2}) = V(\\bar{X_1})+V(\\bar{X_2}) = \\dfrac{\\sigma_{1}^{2}}{n_1}+\\dfrac{\\sigma_{2}^{2}}{n_2}.';
+  z = 'Z = \\dfrac{\\bar{X_1}-\\bar{X_2}-(\\mu_{1}-\\mu_{2})}{\\sqrt{\\dfrac{\\sigma_{1}^{2}}{n_1}+\\dfrac{\\sigma_{2}^{2}}{n_2}}}';
+
+  nmu1 = 'N(\\mu_{1}, \\sigma_{1}^{2} / n_{1})';
+  nmu2 = 'N(\\mu_{2}, \\sigma_{2}^{2} / n_{2})';
+  sigma1n = '\\dfrac{\\sigma_{1}^{2}}{n_1}+\\dfrac{\\sigma_{2}^{2}}{n_2}';
+
   constructor() { }
 
   ngOnInit(): void {
