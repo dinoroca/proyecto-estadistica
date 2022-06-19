2f8ea156ea6d46c7b3aabfc4c8da485187864f37
Tucno0 <yantucnoperez@gmail.com>
2022-06-19 11:37:05 -0500

    Distribuciones de variables aleatorias discretas TERMINADO .I.

--- src/app/pages/inferencia/dist-importantes/vad-poisson/vad-poisson.component.ts
+++ src/app/pages/inferencia/dist-importantes/vad-poisson/vad-poisson.component.ts
@@ -9,6 +9,23 @@ export class VadPoissonComponent implements OnInit {
 
   constructor() { }
 
+  //ecuaciones
+  landa = '\\lambda (\\lambda > 0)';
+  x = 'X \\sim P(\\lambda)';
+
+  funcion = 'f(x) = P[X=x] = \\dfrac{e^{-\\lambda }{(\\lambda)^x}}{x!}, \\     \\ \\ x = 0,1,2,...';
+
+  suma = '\\displaystyle\\sum_{k=0}^{\\infty} \\dfrac{(\\lambda)^k}{k!} = e^k'
+
+  suma2 = '\\displaystyle\\sum_{k=0}^{\\infty}\\dfrac{e^{-\\lambda }{(\\lambda)^x}}{x!} = e^{-\\lambda}\\displaystyle\\sum_{k=0}^{\\infty} \\dfrac{(\\lambda)^k}{k!} = e^{-\\lambda}e^{\\lambda} = e^0 = 1';
+
+  //nota
+  mt = 'mt^3';
+
+  //teorema
+  me = '\\mu = \\lambda';
+  vari = '\\sigma^2 = \\lambda';
+
   ngOnInit(): void {
   }
 
