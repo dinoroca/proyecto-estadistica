c6af3b809b6e325d54520704cc8518833f19cde6
dino-linux <rocalaurented@gmail.com>
2022-06-19 23:14:57 -0500

    Agregado conceptos en inferencia

--- src/app/pages/inferencia/dist-muestrales/dm-dif-dos-varianzas/dm-dif-dos-varianzas.component.ts
+++ src/app/pages/inferencia/dist-muestrales/dm-dif-dos-varianzas/dm-dif-dos-varianzas.component.ts
@@ -26,6 +26,20 @@ export class DmDifDosVarianzasComponent implements OnInit {
   nmu1 = 'N(\\mu_{1}, \\sigma_{1}^{2} / n_{1})';
   nmu2 = 'N(\\mu_{2}, \\sigma_{2}^{2} / n_{2})';
   sigma1n = '\\dfrac{\\sigma_{1}^{2}}{n_1}+\\dfrac{\\sigma_{2}^{2}}{n_2}';
+  nmusigma1 = 'N(\\mu_{1}, \\sigma_{1}^2)';
+  nmusigma2 = 'N(\\mu_{2}, \\sigma_{2}^2)';
+  sigmaigual = '\\sigma_{1}^{2} = \\sigma_{1}^{2} = \\sigma^{2}';
+  sigmadif  = '\\sigma_{1}^{2} != \\sigma_{1}^{2}';
+  nmu12 = 'N \\left(\\mu_{1}-\\mu_{2}, \\dfrac{\\sigma_{1}^{2}}{n_1}+\\dfrac{\\sigma_{2}^{2}}{n_2} \\right)';
+  zx1x2bar = 'Z = \\dfrac{\\bar{X_1}-\\bar{X_2}-(\\mu_{1}-\\mu_{2})}{\\sqrt{\\dfrac{\\sigma_{1}^{2}}{n_1}+\\dfrac{\\sigma_{2}^{2}}{n_2}}} = \\dfrac{\\bar{X_1}-\\bar{X_2}-(\\mu_{1}-\\mu_{2})}{\\sqrt[\\sigma]{\\dfrac{1}{n_1}+\\dfrac{1}{n_2}}}';
+
+  n1men1 = '\\dfrac{(n_{1}-1)\\hat{S}_{1}^{2}}{\\sigma_{1}^{2}} \\sim \\chi^{2}(n_{1}-1)';
+  n2men1 = '\\dfrac{(n_{2}-1)\\hat{S}_{2}^{2}}{\\sigma_{2}^{2}} \\sim \\chi^{2}(n_{2}-1)';
+  v = 'V = \\dfrac{(n_{1}-1)\\hat{S}_{1}^{2}}{\\sigma_{1}^{2}} + \\dfrac{(n_{2}-1)\\hat{S}_{2}^{2}}{\\sigma_{2}^{2}} \\sim \\chi^{2}(n_{1}+n_{2}-2)';
+  tz = 'T=\\dfrac{Z}{\\sqrt{\\dfrac{V}{n_{1}+n_{2}-2}}} = \\dfrac{(\\bar{X}_{1} - \\bar{X}_{2}) -(\\mu_{1} - \\mu_{2})}{\\sqrt{\\dfrac{\\hat{S}_{C}^{2}}{n_1} + \\dfrac{\\hat{S}_{C}^{2}}{n_2}}}';
+
+  sc = '\\hat{S}_{C}^{2}';
+  sc2 = '\\hat{S}_{C}^{2} = \\dfrac{(n_{1}-1)\\hat{S}_{1}^{2} + (n_{2}-1)\\hat{S}_{2}^{2}}{n_{1}+n_{2} -2}';
 
   constructor() { }
 
