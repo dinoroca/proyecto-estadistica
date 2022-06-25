d314a5bf89d4d9e6e7f726c01e2fa654e48b3e47
Yemer <eduardrivera2000@gmail.com>
2022-06-24 22:18:20 -0500

    Lo que me toco
    falta de D. muestral

--- src/app/pages/inferencia/dist-muestrales/ma-estadisticas/ma-estadisticas.component.ts
+++ src/app/pages/inferencia/dist-muestrales/ma-estadisticas/ma-estadisticas.component.ts
@@ -8,6 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class MaEstadisticasComponent implements OnInit {
 
   constructor() { }
+//Ecuaciones
+  Y = 'Y = H(X_1,X_2,...,X_n)';
+  y = 'y = H(x_1,x_2,...,x_n)';
+  medM = '\\overline{X}=\\displaystyle\\frac{1}{n}\\sum_{i=1}^{n}X_i';
+  medm = '\\overline{x}=\\displaystyle\\frac{1}{n}\\sum_{i=1}^{n}x_i';
+  varM = 'S^2=\\displaystyle\\frac{1}{n}\\sum_{i=1}^{n}(X_i-\\overline{X})^2';
+  varm = 's^2=\\displaystyle\\frac{1}{n}\\sum_{i=1}^{n}(x_i-\\overline{x})^2';
+  desE = 's=\\sqrt{s^2}';
+  P = '\\widehat{P}';
+  P1 = '\\overline{P}';
+  p = 'p';
+  p1 = '\\overline{p}'
+  p2 = '\\widehat{p}';
+  pb = '\\overline{P}=\\displaystyle\\frac{1}{n}\\sum_{i=1}^{n}X_i';
+  Pb = '\\overline{P}=\\displaystyle\\frac{X}{n}, donde  X \\sim B(n,p)';
+  X = 'X_i \\sim B(1,p)'
+
+
+
+
+
 
   ngOnInit(): void {
   }
--- src/app/pages/inferencia/dist-muestrales/ma-tipos-muestra/ma-tipos-muestra.component.ts
+++ src/app/pages/inferencia/dist-muestrales/ma-tipos-muestra/ma-tipos-muestra.component.ts
@@ -8,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class MaTiposMuestraComponent implements OnInit {
 
   constructor() { }
-
+  //Ecuaciones
+  Ec1 = 'X_i';
+  Ec2 = 'x_i';
+  Ec3 = 'x_1,x_2,...,x_n';
+  Ec4 = 'X_1,X_2,...,X_n';
+  fx = 'f(x)';
+  X = 'X';
+  mu ='\\mu';
+  Var = '\\sigma^2';
+  n ='n';
+  fx1 ='f(x_1,x_2,...,x_n)=f(x_1)f(x_2),...,f(x_n)';
+  media = 'E(X_i)=\\mu';
+  Varianza = 'Var(X_i)=\\sigma^2';
+  fxi = 'f(x_i)=f(x)';
   ngOnInit(): void {
   }
 
