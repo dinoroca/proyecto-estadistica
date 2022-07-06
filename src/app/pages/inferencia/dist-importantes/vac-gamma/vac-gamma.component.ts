1ddc10e56f54ede75dd4d8685a64cf1dd6a41786
jamilmendez <mdztnr99@gmail.com>
2022-07-06 00:29:44 -0500

    distribución exponencial agregado al 90%

--- src/app/pages/inferencia/dist-importantes/vac-gamma/vac-gamma.component.ts
+++ src/app/pages/inferencia/dist-importantes/vac-gamma/vac-gamma.component.ts
@@ -12,21 +12,20 @@ export class VacGammaComponent implements OnInit {
     this.title.setTitle('Inferencia | v.a.c. Gamma');
   }
 
-  constructor( private title: Title ) {}
+  constructor(private title: Title) { }
 
   // variables
   x = 'X';
-  xminus = 'x';
-  alpha ='\\alpha';
+  alpha = '\\alpha';
   beta = '\\beta';
   dgamma = `X \\sim \\Gamma(${this.alpha}, ${this.beta})`;
   fundensidgamma = `f(x) = \\left\\{\\begin{array}{lcc}
-  \\dfrac{${this.beta}^${this.alpha}}{\\Gamma(${this.alpha})}x^{\\alpha-1}e^{-\\beta x}, & si & x\\geq0\\\\
-  \\\\ 0, & si & x<1\\\\
-  \\end{array}
-  \\right.`;
+    \\dfrac{${this.beta}^${this.alpha}}{\\Gamma(${this.alpha})}x^{\\alpha-1}e^{-\\beta x}, & si & x\\geq0\\\\
+    \\\\ 0, & si & x<1\\\\
+    \\end{array}
+    \\right.`;
   medg = '\\mu=\\dfrac{\\alpha}{\\beta}';
   varg = '\\sigma^2=\\dfrac{\\alpha}{\\beta^2}';
-  
+
 
 }
